bbe1207a5617c65c89cf6ef292ddc90f2f885ee0
Andy <ajemedafe@gmail.com>
2023-11-16 12:38:39 +0000

    Add excercoseRouter to server.js

---
+++ wrist-watch-server/controller/excerciseController.js
@@ -0,0 +1,4 @@
+const router = require("express").Router();
+const knex = require("knex")(require("../knexfile"));
+
+module.exports = {};
--- wrist-watch-server/server.js
+++ wrist-watch-server/server.js
@@ -2,10 +2,11 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const port = process.env.PORT;
-console.log(port);
+const excerciseRouter = require("./routes/excerciseRouter");
 
 app.use(express.json());
 app.use(cors());
+app.use("/excercises", excerciseRouter);
 
+const port = process.env.PORT || 8182;
 app.listen(port, () => console.log(`Server running on port ${port}`));
